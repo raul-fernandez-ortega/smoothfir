36cb85a44f8dfb6d3ea539cd34262a0488b959c1
raul-fernandez-ortega <42633165+raul-fernandez-ortega@users.noreply.github.com>
2022-08-05 17:59:25 +0200

    Add files via upload

--- run_DRCoP.py
+++ run_DRCoP.py
@@ -4,7 +4,7 @@ import sys
 import time
 import os
 import smoothfir
-import thread
+#import thread
 
 IN = 0
 OUT = 1
@@ -13,15 +13,15 @@ def print_filter_output_att(agent, config):
     outch = agent.getChannels(OUT)
     filters = agent.getFilterData()
     for n in range(config.n_filters):
-        print "Filter number:",n
+        print("Filter number:",n)
         bfilter = filters[n] 
-        print "Name:",bfilter.name
+        print("Name:",bfilter.name)
         fcontrol = agent.getFilterControl(n)
         outp_index = 0
-        print "Outputs:", agent.getFilternChannels(bfilter,1)
+        print("Outputs:", agent.getFilternChannels(bfilter,1))
         for outp in agent.getFilterChannels(bfilter,1):
-            print outp, outch[outp].name
-            print "Output attenuation: %.1f dB."%agent.get_filter_attenuation(bfilter.name, OUT, outp_index)
+            print(outp, outch[outp].name)
+            print("Output attenuation: %.1f dB."%agent.get_filter_attenuation(bfilter.name, OUT, outp_index))
             outp_index +=1
 
 def print_filter_data(agent, config):
@@ -29,40 +29,40 @@ def print_filter_data(agent, config):
     outch = agent.getChannels(OUT)
     filters = agent.getFilterData()
     for n in range(config.n_filters):
-        print "Filter number:",n
+        print("Filter number:",n)
         bfilter = filters[n] 
-        print "Name:",bfilter.name
+        print("Name:",bfilter.name)
         fcontrol = agent.getFilterControl(n)
         if fcontrol.coeff > -1:
-            print "Coeff. Index:",fcontrol.coeff, "Name:", config.coeffs[fcontrol.coeff].name
+            print("Coeff. Index:",fcontrol.coeff, "Name:", config.coeffs[fcontrol.coeff].name)
         else:
-            print "No Coeff Index."
-        print "Inputs:",agent.getFilternChannels(bfilter,0)
+            print("No Coeff Index.")
+        print("Inputs:",agent.getFilternChannels(bfilter,0))
         inp_index = 0
         for inp in agent.getFilterChannels(bfilter,0):
-            print inp, inch[inp].name
-            print "Input attenuation: %.1f dB."%agent.get_filter_attenuation(bfilter.name, IN, inp_index)
+            print(inp, inch[inp].name)
+            print("Input attenuation: %.1f dB."%agent.get_filter_attenuation(bfilter.name, IN, inp_index))
             inp_index +=1
-        print "Outputs:", agent.getFilternChannels(bfilter,1)
+        print("Outputs:", agent.getFilternChannels(bfilter,1))
         outp_index = 0
         for outp in agent.getFilterChannels(bfilter,1):
-            print outp, outch[outp].name
-            print "Output attenuation: %.1f dB."%agent.get_filter_attenuation(bfilter.name, OUT, outp_index)
+            print(outp, outch[outp].name)
+            print("Output attenuation: %.1f dB."%agent.get_filter_attenuation(bfilter.name, OUT, outp_index))
             outp_index +=1
-        print "Filter Inputs:",agent.getFilternFilters(bfilter,0)
+        print("Filter Inputs:",agent.getFilternFilters(bfilter,0))
         for finp in agent.getFilterFilters(bfilter,0):
-            print finp, filters[finp].name
-        print "Filter Outputs:",agent.getFilternFilters(bfilter,1)
+            print(finp, filters[finp].name)
+        print("Filter Outputs:",agent.getFilternFilters(bfilter,1))
         for foutp in agent.getFilterFilters(bfilter,1):
-            print foutp, filters[foutp].name      
-        print "=============================================="
+            print(foutp, filters[foutp].name)
+        print("==============================================")
 
 class smoothfir_exec:
     
     def __init__(self, xml_config_file):
-        print "Smoothfir object creation."
+        print("Smoothfir object creation.")
         self.convolver = smoothfir.smoothfir()
-        print "Loading smoothfir config." 
+        print("Loading smoothfir config.")
         self.convolver.sfConf.sfconf_init(xml_config_file)
         # link to smoothfir configuration data
         self.config = self.convolver.sfConf.sfconf
@@ -71,9 +71,9 @@ class smoothfir_exec:
         self.convolver.sfConf.add_sflogic(self.agent)
         
     def start(self):
-        print "Starting smoothfir." 
+        print("Starting smoothfir.")
         self.convolver.sfRun.sfrun(self.convolver.sfConf.sfConv, self.convolver.sfConf.sfDelay, self.convolver.sfConf.sflogic)
-        print "Smoothfir is running..."
+        print("Smoothfir is running...")
         
     def print_data(self):
         print_filter_data(self.agent, self.config)
@@ -84,7 +84,7 @@ class smoothfir_exec:
 def main(configFile):
     smoothfirRun = smoothfir_exec(configFile)
     smoothfirRun.start()
-    print "Smoothfir is running..."
+    print("Smoothfir is running...")
     smoothfirRun.print_data()
     DT = time.time()
 
@@ -93,7 +93,7 @@ def main(configFile):
         #smoothfirRun.print_output_att()
         #agent.change_input_attenuation('left',2.0)
         #agent.change_input_attenuation('right',3.0)
-        print "Running time: %d s."%(time.time()-DT)
+        print("Running time: %d s."%(time.time()-DT))
 
 if __name__ == "__main__":
     main(sys.argv[1])
